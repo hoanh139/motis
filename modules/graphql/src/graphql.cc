fb96346fa8e1361044d195e59675d86fbe2b9526
Le <hahoanganhle139@gmail.com>
2023-08-04 13:42:02 +0200

    anderung am buro

--- modules/graphql/src/graphql.cc
+++ modules/graphql/src/graphql.cc
@@ -1,7 +1,13 @@
 #include "motis/graphql/graphql.h"
 
+#include "graphqlservice/JSONResponse.h"
+#include "otp/QueryTypeObject.h"
+#include "otp/TripObject.h"
+#include "otp/otpSchema.h"
+
 namespace mm = motis::module;
 namespace fbb = flatbuffers;
+namespace gql = graphql;
 
 namespace motis::graphql {
 
@@ -12,6 +18,11 @@ void graphql::init(motis::module::registry& reg) {
       "/graphql",
       [](mm::msg_ptr const& msg) {
         auto const req = motis_content(HTTPRequest, msg);
+
+        auto const query = gql::peg::parseString(req->content()->str());
+        gql::otp::Operations{std::make_shared<gql::otp::object::QueryType>(
+            []() { return std::make_shared<gql::otp::object::Trip>(); })};
+
         mm::message_creator mc;
         mc.create_and_finish(
             MsgContent_HTTPResponse,
