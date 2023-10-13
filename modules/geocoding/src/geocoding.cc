1a207be522ab55517680587ccf51c2e689c7af53
Le <hahoanganhle139@gmail.com>
2023-10-13 13:30:59 +0200

    Geocoding modul erstellt

--- modules/geocoding/src/geocoding.cc
+++ modules/geocoding/src/geocoding.cc
@@ -193,7 +193,115 @@ void geocoding::init(motis::module::registry& reg) {
         //        geocodingAddressViaPosition(req->content()->str()); auto const
         //        station_res =
         //            motis_content(StationGuesserResponse, station_msg);
-        
+
+        auto from =
+            "{\n"
+            "\t\"features\": [\n"
+            "\t\t{\n"
+            "\t\t\t\"geometry\": {\n"
+            "\t\t\t\t\"coordinates\": [\n"
+            "\t\t\t\t\t6.105464,\n"
+            "\t\t\t\t\t50.758075\n"
+            "\t\t\t\t],\n"
+            "\t\t\t\t\"type\": \"Point\"\n"
+            "\t\t\t},\n"
+            "\t\t\t\"type\": \"Feature\",\n"
+            "\t\t\t\"properties\": {\n"
+            "\t\t\t\t\"osm_id\": 25416882,\n"
+            "\t\t\t\t\"extent\": [\n"
+            "\t\t\t\t\t6.105464,\n"
+            "\t\t\t\t\t50.758075\n"
+            "\t\t\t\t],\n"
+            "\t\t\t\t\"country\": \"Deutschland\",\n"
+            "\t\t\t\t\"city\": \"Aachen\",\n"
+            "\t\t\t\t\"countrycode\": \"DE\",\n"
+            "\t\t\t\t\"county\": \"Landkreis Aachen\",\n"
+            "\t\t\t\t\"type\": \"street\",\n"
+            "\t\t\t\t\"osm_type\": \"W\",\n"
+            "\t\t\t\t\"osm_key\": \"highway\",\n"
+            "\t\t\t\t\"district\": \"Beverau\",\n"
+            "\t\t\t\t\"osm_value\": \"track\",\n"
+            "\t\t\t\t\"name\": \"Petronellastraße\",\n"
+            "\t\t\t\t\"region\": \"Nordrhein-Westfalen\",\n"
+            "\t\t\t\t\"postalcode\": \"52066\",\n"
+            "\t\t\t\t\"locality\": \"Aachen\",\n"
+            "\t\t\t\t\"label\": \"Petronellastraße 6, 52066 Aachen\",\n"
+            "\t\t\t\t\"layer\": \"venue\",\n"
+            "\t\t\t\t\"confidence\": 1,\n"
+            "\t\t\t\t\"source\": \"openstreetmap\"\n"
+            "\t\t\t}\n"
+            "\t\t}\n"
+            "\t]\n"
+            "}";
+        auto to =
+            "{\n"
+            "\t\"features\": [\n"
+            "\t\t{\n"
+            "\t\t\t\"geometry\": {\n"
+            "\t\t\t\t\"coordinates\": [\n"
+            "\t\t\t\t\t6.07519,\n"
+            "\t\t\t\t\t50.782213999999996\n"
+            "\t\t\t\t],\n"
+            "\t\t\t\t\"type\": \"Point\"\n"
+            "\t\t\t},\n"
+            "\t\t\t\"type\": \"Feature\",\n"
+            "\t\t\t\"properties\": {\n"
+            "\t\t\t\t\"osm_id\": 25416882,\n"
+            "\t\t\t\t\"extent\": [\n"
+            "\t\t\t\t\t6.07519,\n"
+            "\t\t\t\t\t50.782213999999996\n"
+            "\t\t\t\t],\n"
+            "\t\t\t\t\"country\": \"Deutschland\",\n"
+            "\t\t\t\t\"city\": \"Aachen\",\n"
+            "\t\t\t\t\"countrycode\": \"DE\",\n"
+            "\t\t\t\t\"county\": \"Landkreis Aachen\",\n"
+            "\t\t\t\t\"type\": \"street\",\n"
+            "\t\t\t\t\"osm_type\": \"W\",\n"
+            "\t\t\t\t\"osm_key\": \"highway\",\n"
+            "\t\t\t\t\"district\": \"Mitte Aachen\",\n"
+            "\t\t\t\t\"osm_value\": \"track\",\n"
+            "\t\t\t\t\"name\": \"Brüggemannstraße\",\n"
+            "\t\t\t\t\"region\": \"Nordrhein-Westfalen\",\n"
+            "\t\t\t\t\"postalcode\": \"52072\",\n"
+            "\t\t\t\t\"locality\": \"Aachen\",\n"
+            "\t\t\t\t\"label\": \"Brüggemannstraße 4, 52072 Aachen\",\n"
+            "\t\t\t\t\"layer\": \"venue\",\n"
+            "\t\t\t\t\"confidence\": 1,\n"
+            "\t\t\t\t\"source\": \"openstreetmap\"\n"
+            "\t\t\t}\n"
+            "\t\t}\n"
+            "\t]\n"
+            "}";
+        std::string MsgSend = from;
+        if (changeAtr) {
+          MsgSend = to;
+          changeAtr = !changeAtr;
+        } else {
+          MsgSend = from;
+          changeAtr = !changeAtr;
+        }
+        mm::message_creator mc;
+        mc.create_and_finish(
+            MsgContent_HTTPResponse,
+            CreateHTTPResponse(
+                mc, HTTPStatus_OK,
+                mc.CreateVector(std::vector<fbb::Offset<HTTPHeader>>{}),
+                mc.CreateString(MsgSend))
+                .Union());
+        return make_msg(mc);
+      },
+      {});
+  reg.register_op(
+      "/geocoding/search",
+      [](mm::msg_ptr const& msg) {
+        //        auto const req = motis_content(HTTPRequest, msg);
+
+        //        auto response = req->content()->str();
+        //        auto station_msg =
+        //        geocodingAddressViaPosition(req->content()->str()); auto const
+        //        station_res =
+        //            motis_content(StationGuesserResponse, station_msg);
+
         auto from =
             "{\n"
             "\t\"features\": [\n"
